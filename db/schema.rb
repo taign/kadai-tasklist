daacb71415123973ee2ac77dd67ac86446a17a5f
taisuke oosumi <1112xstaobiauwb@gmail.com>
2017-04-21 05:34:45 +0000

    lesson9 11 kadai

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170420042933) do
+ActiveRecord::Schema.define(version: 20170421044302) do
 
   create_table "tasks", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "content"
+    t.string   "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "title"
   end
 
 end
