573ff1265954d458a36e11eacb5d60f882c3e786
taisuke oosumi <1112xstaobiauwb@gmail.com>
2017-04-24 00:29:58 +0000

    init Rails Project

--- README.md
+++ README.md
@@ -25,3 +25,4 @@ Things you may want to cover:
 # kadai-tasklist
 # kadai-tasklist
 # kadai-tasklist
+# kadai-tasklist
--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -16,10 +16,10 @@ class TasksController < ApplicationController
     @task = Task.new(task_params)
 
     if @task.save
-      flash[:success] = 'Task が正常に投稿されました'
+      flash[:success] = 'タスク が正常に投稿されました'
       redirect_to @task
     else
-      flash.now[:danger] = 'Task が投稿されませんでした'
+      flash.now[:danger] = 'タスク が投稿されませんでした'
       render :new
     end
   end
@@ -29,10 +29,10 @@ class TasksController < ApplicationController
 
   def update
     if @task.update(task_params)
-      flash[:success] = 'Task は正常に更新されました'
+      flash[:success] = 'タスク は正常に更新されました'
       redirect_to @task
     else
-      flash.now[:danger] = 'Task は更新されませんでした'
+      flash.now[:danger] = 'タスク は更新されませんでした'
       render :edit
     end
   end
@@ -40,7 +40,7 @@ class TasksController < ApplicationController
   def destroy
     @task.destroy
 
-    flash[:success] = 'Task は正常に削除されました'
+    flash[:success] = 'タスク は正常に削除されました'
     redirect_to tasks_url
   end
 
